7b7a7c0e43f14a589dd127dda954a5b6625e87d3
BisalDangol <message2bishal@outlook.com>
2025-01-31 21:23:37 +0545

    form valadatation

--- login/form/views.py
+++ login/form/views.py
@@ -9,12 +9,14 @@ def form(request):
         last_name = request.POST.get("last_name")
         email = request.POST.get("email")
         phone_number = request.POST.get("phone_number")
-        if len(phone_number) !=10 or phone_number[0]==0:
+        if len(phone_number) !=10 or phone_number[0]==0 or phone_number[0] != 9 :
             return HttpResponse("Number should be exact 10 and should not start from Zero")
         book = request.POST.get("book")
         slug = request.POST.get("slug")
         if not slug:
             slug = slugify (f"{first_name}-{last_name}")
+        
+        
 
         student = Student(first_name=first_name, last_name=last_name, email=email,phone_number=phone_number,book=book)
         student.save()
